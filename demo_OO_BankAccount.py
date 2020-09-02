849bdd353de3ddb3f18cfe1cc26fe03f4da679ae
Peter Anema <7320173+PeterAnema@users.noreply.github.com>
2020-09-02 15:23:18 +0200

    Update

--- demo_OO_BankAccount.py
+++ demo_OO_BankAccount.py
@@ -1,8 +1,9 @@
 
 class BankAccount(object):
 
-    def __init__(self, name):
-        self.accountHolder = name
+    def __init__(self, iban, name):
+        self.__iban = iban
+        self.__account_holder = name
         self.__balance = 0.0
 
     def deposit(self, amount):
@@ -12,15 +13,18 @@ class BankAccount(object):
         self.__balance -= amount
 
     def info(self):
-        print('%s: %.2f' % (self.accountHolder, self.__balance))
+        print('Account %s of %s has a balance of €%.2f' % (self.__iban,
+                                                           self.__account_holder,
+                                                           self.__balance))
 
-#--------------------------------------
 
-acc1 = BankAccount('Fahad')
+# --------------------------------------
+
+acc1 = BankAccount('NL86INGB0564654689', 'Fahad')
 acc1.deposit(200)
 acc1.withdraw(50)
 
-acc2 = BankAccount('Peter')
+acc2 = BankAccount('NL45ABCA0354253678', 'Peter')
 acc2.deposit(1000)
 acc2.withdraw(100)
 
